e150397e18adc750e2ce16e7de04b2cbe3f6cd06
lovesossa <qwertyerku5@gmail.com>
2023-10-24 15:45:37 +0300

    refactor(dev scripts)

--- dev-scripts.js
+++ dev-scripts.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const createSitemap = () => {
 	const PAGES_DIR = `${path.resolve(__dirname, 'src')}/pug/pages/`;
