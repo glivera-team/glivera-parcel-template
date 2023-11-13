e07c4b1f1dc4fa99647d974758e6e2e0b47a0974
lovesossa <qwertyerku5@gmail.com>
2023-11-13 11:04:04 +0200

    refactor(ui, dev widget)

---
+++ dev-scripts.mjs
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename);
+
+const createSitemap = () => {
+	const PAGES_DIR = `${path.resolve(__dirname, 'src')}/pug/pages/`;
+	const pagesArray = fs
+		.readdirSync(PAGES_DIR)
+		.filter((fileName) => fileName.endsWith('.pug'))
+		.map((page) => `'${page.split('.').slice(0, -1).join('.')}'`);
+	// SITEMAP.split('.').slice(0, -1).join('.');
+
+	const directoryPath = './src/pug/templates/global-vars.pug';
+	const sitemapVariableStr = `\n- _sitemap = [${pagesArray}];`;
+
+	fs.readFile(directoryPath, 'utf-8', (err, oldContent) => {
+		if (err) return;
+		const newContent = oldContent.replace(/(private_vars).*/gs, '$1');
+		fs.writeFile(directoryPath, newContent.concat(sitemapVariableStr), 'utf-8', () => undefined);
+	});
+};
+
+createSitemap();
--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import './scss/main-global.scss';
 // js
 import { documentReady } from 'utils';
 import { ENV_STATUS } from 'utils/constants';
-import pageWidgetInit from 'dev-vendors/dev-widget';
 import app from './js/app';
 
 const styles = ['color: #fff', 'background: #cf8e1f'].join(';');
 const message = 'Developed by Glivera-team https://glivera-team.com/';
 // eslint-disable-next-line no-console
 console.info('%c%s', styles, message);
-
-// -------------------  dev widget
-if (ENV_STATUS.projectDevStatus || ENV_STATUS.projectStagingStatus) {
-	pageWidgetInit();
-}
-// -------------------  dev widget###
 
 // -------------------  init App
 documentReady(() => {
