355dad9ed2eb697e918a3b34b2cf2e02e9736ec7
lovesossa <qwertyerku5@gmail.com>
2023-12-22 15:32:15 +0200

    test  deploy with sprite

--- pug.config.js
+++ pug.config.js
@@ -1,7 +1,12 @@
 const { readFileSync } = require('fs');
+const { resolve } = require('path');
+
+const directoryUrl = __dirname;
 
 module.exports = {
 	locals: {
 		readFileSync,
+		resolve,
+		directoryUrl,
 	},
 };
