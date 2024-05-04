22d2c1e22be1e1d9832218a01b5079c310b0251e
lovesossa <geoshavel@gmail.com>
2024-05-04 15:09:05 +0300

    feat( resize event, exist check feature )

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,16 @@ import { documentReady } from 'utils';
 import { ENV_STATUS } from 'utils/constants';
 import app from './app';
 
+// --------------------------------------------- native javascript extends
+
+window.NodeList.prototype.map = Array.prototype.map;
+window.NodeList.prototype.filter = Array.prototype.filter;
+window.Object.prototype.exist = function () {
+	// check if array empty or boolean check for other types
+	return Array.isArray(this) ? this?.length > 0 : !!this;
+};
+// --------------------------------------------- native javascript extends###
+
 const styles = ['color: #fff', 'background: #cf8e1f'].join(';');
 const message = 'Developed by Glivera-team https://glivera-team.com/';
 // eslint-disable-next-line no-console
