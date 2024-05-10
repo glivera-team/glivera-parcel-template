55def4a117cabe3f3c9a874c6b25a0f771fa2dfe
lovesossa <geoshavel@gmail.com>
2024-05-10 17:25:57 +0300

    refactor build scripts, staging: last deploy version in console

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,11 @@ import '../scss/main-global.scss';
 
 // js
 import { documentReady } from 'utils';
-import { ENV_STATUS } from 'utils/constants';
 import app from './app';
 
 const styles = ['color: #fff', 'background: #cf8e1f'].join(';');
 const message = 'Developed by Glivera-team https://glivera-team.com/';
+
 // eslint-disable-next-line no-console
 console.info('%c%s', styles, message);
 
